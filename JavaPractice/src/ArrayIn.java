d2889232ff3c79d26e627b35e902d92f46178e8a
VINAYVARMAGIT <169148572+VINAYVARMAGIT@users.noreply.github.com>
2024-05-21 21:35:12 +0530

    remo

--- JavaPractice/src/ArrayIn.java
+++ JavaPractice/src/ArrayIn.java
@@ -5,7 +5,7 @@ public class ArrayIn {
     public static void main(String[] args) {
         System.out.println(num[2]);
         // this is the newly added file 
-        //another libne added 
+        
     }
 
 }
