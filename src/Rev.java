bb620c4d056d5a2dd95620197ad62ae43d98f2da
VINAYVARMAGIT <169148572+VINAYVARMAGIT@users.noreply.github.com>
2024-05-23 23:04:52 +0530

    added some code

--- src/Rev.java
+++ src/Rev.java
@@ -9,21 +9,32 @@ public class Rev {
         Character[] characters = {'s', 'v', 'i'};
 
         List<Character> list1 = Arrays.asList(characters);
-        System.out.println("Original list: " + list1);
+        //System.out.println("Original list: " + list1);
+        output(list1);
 
         Collections.reverse(list1);
-        System.out.println("Reversed list: " + list1);
-       
+       // System.out.println("Reversed list: " + list1);
+       output(list1);
         Character[] newCharacters = new Character[3];
         List<Character> list2 = Arrays.asList(newCharacters);
 
         Collections.copy(list2, list1);
+        output(list2);
+        // System.out.println("copied list:" + list2);
+          
+        Collections.fill(list1, 'x');
+        output(list1);
+    }
 
-        System.out.println("copied list:" + list2);
+    private static void output  (List<Character> thelist){
+
+        for(Character things : thelist)
+        System.out.printf("%s ", things);
+
+        System.out.println();
         
-      
-    
-    
-    
+
+
+        
     }
 }
