2cd7b1fe945169bd6a3109aceaf6b7ea4a94d709
VINAYVARMAGIT <169148572+VINAYVARMAGIT@users.noreply.github.com>
2024-05-24 14:45:44 +0530

    post pre increments and decrements

---
+++ src/PrePost.java
@@ -0,0 +1,26 @@
+public class PrePost {
+
+    public static void main(String[] args) {
+        
+    
+    
+        int n = 5;
+        //n = n++;
+        
+
+    System.out.println(n++);
+    System.err.println(n);
+   
+    System.out.println(n++);
+    System.err.println(n);
+    
+    System.out.println(n++);
+    System.err.println(n);
+    
+
+
+
+
+    }
+
+}
