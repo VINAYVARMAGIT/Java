53fd2f2c4ca633578f5610110a5694c91b47de32
VINAYVARMAGIT <169148572+VINAYVARMAGIT@users.noreply.github.com>
2024-06-08 15:05:29 +0530

    NumberTraingleReverse

--- src/NumberTriangler.java
+++ src/NumberTriangler.java
@@ -1,19 +1,40 @@
 import java.util.Scanner;
 
+// public class NumberTriangler {
+//     public static void main(String[] args) {
+//         Scanner s = new Scanner(System.in);
+//         int n = s.nextInt();
+
+//         for (int i = 1; i <=n; i++) {
+
+
+//             for (int j = 0; j <n-i ; j++) {
+//                 System.out.print(" ");              
+//             }
+
+//             for (int j = 1; j <=i ; j++) {
+//                 System.out.print(i +" ");
+//             }
+//             System.out.println("");
+            
+//         }
+//     }
+
+// }
+
 public class NumberTriangler {
     public static void main(String[] args) {
         Scanner s = new Scanner(System.in);
         int n = s.nextInt();
 
-        for (int i = 1; i <=n; i++) {
-
-
-            for (int j = 0; j <n-i ; j++) {
-                System.out.print(" ");              
-            }
+        for (int i = 1; i <= n; i++) {
 
             for (int j = 1; j <=i ; j++) {
-                System.out.print(i +" ");
+                System.out.print(" ");
+            }
+            
+            for (int j = 0; j <=n-i ; j++) {
+                System.out.print(i +" ");              
             }
             System.out.println("");
             
