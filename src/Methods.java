87ca813893698dcc764a4c0b4b05289935662adc
VINAYVARMAGIT <169148572+VINAYVARMAGIT@users.noreply.github.com>
2024-06-13 16:19:53 +0530

    Methods file

--- src/Methods.java
+++ src/Methods.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public class Methods {
+class Methods {
 
     public static void main(String[] args) {
         
@@ -9,7 +9,7 @@ public class Methods {
 
         if (n <=1) {
             System.out.println("It's not a prime number.");
-        //return;
+        return;
         }
         for (int i = 2; i <=n/2; i++) {
             if (n%i==0) {
@@ -23,3 +23,102 @@ public class Methods {
 
     }
 
+class A{
+    public static void main(String[] args) {
+        m1(5);
+
+        m1(5+7+2);
+
+        m1('a'); //auto widening
+        m2();
+       // m1(34.24); compile type error
+    }
+
+    static void m1 (int a){
+     System.out.println("a="+ a);
+    }
+
+    static int m2(){
+        {
+            if (true) {
+                //return 5;
+            }
+            //return 7;
+            //System.out.println("This line may not execute.");
+            return 7;
+        }
+    }
+}
+
+class B{
+    public static void main(String[] args) {
+       int a =3;
+       m1(a);
+       System.out.println(a);
+    }
+
+    static void m1(int a){
+        a *=3;
+        System.out.println(a);
+    }
+}
+class C{
+    public static void main(String[] args) {
+        int i=0; //1 2  3
+        int j=0;//9
+             //  2      2        4
+        j = m1(++i)+m1(i++)+m1(++i);
+        System.out.println(i);
+        System.out.println(j);
+    }
+  static int m1(int a)
+  {
+    return ++a;
+  }
+}
+
+class D{
+    public static void main(String[] args) {
+        Scanner s = new Scanner(System.in);
+        System.out.println("Enter the start num:");
+        int i = s.nextInt();
+        System.out.println("Enter the ending num:");
+        int n = s.nextInt();
+        System.out.println(m1(i,n));       
+    }
+    static int m1(int i,int n){
+        if(n <i) return 1;
+         return n*m1(i,n-1);
+        }
+}
+
+
+class E{
+    public static void main(String[] args) {
+        Scanner s = new Scanner(System.in);
+        System.out.println("Enter the num:");
+        int n = s.nextInt();
+        System.out.println(m1(n));
+        m1(n);        
+    }
+    static int m1(int i){
+        if(i ==0) return 0;
+         return i+m1(i-1);
+        }
+}
+
+class F{
+    public static void main(String[] args) {
+
+        int i=2;
+        m1(i);
+    }
+
+    static void m1(int i){
+        if (i>=3)return;
+        System.out.println(i);
+        m1(++i);
+        m1(++i);
+        System.out.println(i);
+    }
+}
