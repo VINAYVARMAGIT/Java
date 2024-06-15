91dff776a37f9818d67d9dced0b685065d27698a
VINAYVARMAGIT <169148572+VINAYVARMAGIT@users.noreply.github.com>
2024-06-15 21:15:44 +0530

    Recursion with methods

--- src/Methods.java
+++ src/Methods.java
@@ -122,3 +122,4 @@ class F{
         System.out.println(i);
     }
 }
+
---
+++ src/UsingMethodRecursion.java
@@ -0,0 +1,54 @@
+import java.util.Scanner;
+
+public class UsingMethodRecursion {
+    public static void main(String[] args) {
+        
+        Scanner s = new Scanner(System.in);
+        int n = s.nextInt();
+        boolean isPrime =m1(n,2);
+        
+        if (isPrime) {
+            System.out.println("It's not a prime number");
+        }
+        else System.out.println("It's a prime number");
+
+    }
+
+static boolean m1(int n,int i){
+    if (n==i) {
+        return false;
+    }
+   if (n==0 || n==1) {
+    //System.out.println("It's not a prime number.");
+    return true;
+   }
+  if (n%i == 0)
+   {
+   // System.out.println("It's not a prime number.");
+    return true;
+    }
+    return m1(n, i+1);  
+    }
+}
+
+
+class PalindromeUsingMethods{
+
+    public static void main(String[] args) {
+        Scanner s =new Scanner(System.in);
+        System.out.println("Enter the num:");
+        int n = s.nextInt();
+        int rev = reversed(n,0);
+        if (n == rev) {
+            System.out.println("Its a palindrome number.");     
+        }
+        else System.out.println("It's not a palindrome number");
+    }
+
+    static int reversed(int n,int rev){
+        if (n==0) return rev;
+        int rem =n%10;
+        rev = rev*10 + rem;
+        return reversed(n/10,rev);
+    }
+}
