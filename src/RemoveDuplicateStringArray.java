a3f088b3ad1d5f35d7e2c3451ab91e035c1f5049
VINAYVARMAGIT <vinayvarma0531@gmail.com>
2025-03-20 19:28:26 +0530

    Check First 3 matches String

--- src/RemoveDuplicateStringArray.java
+++ src/RemoveDuplicateStringArray.java
@@ -1,5 +1,7 @@
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.HashSet;
+import java.util.List;
 import java.util.Set;
 
 class RemoveDuplicateStringArray{
@@ -30,6 +32,29 @@ class RemoveDuplicateStringArray{
         String s3[]= new String[set.size()];
         int index=0;
         for (String string : set) {
+            s3[index++]=string;
+        }
+        return s3;
+    }
+}
+class FirstThree{
+    public static void main(String[] args) {
+        String s1[]={"vinay","varma","siddu","sid","vinner","vinni"};
+        String s2[]= getOnlyVin(s1);
+        for (String string : s2) {
+            System.out.print(string+" ");
+        }
+    }
+    public static String[] getOnlyVin(String []  s1){
+        List<String>  l = new ArrayList<>();
+        for (int i = 0; i < s1.length; i++) {
+            if (s1[i].charAt(0)=='v'&&s1[i].charAt(1)=='i'&&s1[i].charAt(2)=='n') {
+                l.add(s1[i]);
+            }
+        }
+        String s3[]= new String[l.size()];
+        int index=0;
+        for (String string : l) {
             s3[index++]=string;
         }
         return s3;
